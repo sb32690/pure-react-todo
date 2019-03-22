37824e7c46d8d3d4a0fb3f05f8e1c036f3d8cee6
Shaun Baxley <42102713+sb32690@users.noreply.github.com>
2019-03-22 15:48:40 -0400

    Fixed issue

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
-const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-}
+var config = {
+  apiKey: "AIzaSyC2k0vvK6MsScduzIbFhaUvGRVknYKTwVk",
+  authDomain: "react-template-firedux-todo.firebaseapp.com",
+  databaseURL: "https://react-template-firedux-todo.firebaseio.com",
+  projectId: "react-template-firedux-todo",
+  storageBucket: "react-template-firedux-todo.appspot.com",
+  messagingSenderId: "214493384373"
+};
 firebase.initializeApp(config);
 const databaseRef = firebase.database().ref();
 //allows firebase to work with redux
